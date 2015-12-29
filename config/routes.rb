fe8eee53882b710d4b6b70b7f4373f7eab24059d
Byron Roark <byronroark@gmail.com>
2015-12-29 15:32:37 -0500

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get '/about', to: 'pages#about', as: :about
   get '/contact_us', to: 'pages#contact', as: :contact
   get '/services', to: 'pages#services', as: :services
+  get '/admins/sign_in', to: 'devise/sessions#new', as: :login
 
   resources :work_orders
 end
